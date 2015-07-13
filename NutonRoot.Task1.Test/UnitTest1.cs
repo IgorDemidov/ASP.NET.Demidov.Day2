8a5b154cc35ef01ad81559ddf6ff9e1b48871231
IgorDemidov <igr.demidov@gmail.com>
2015-07-14 01:55:15 +0300

    Beginning

---
+++ NutonRoot.Task1.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NutonRoot.Task1.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
