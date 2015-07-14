5dc5e9dd4799b0a4226dcb238d6cca592bb5d3a0
IgorDemidov <igr.demidov@gmail.com>
2015-07-14 19:28:04 +0300

    tobecontinue..

--- CustomFormatProvider.Task2.Library/CustomFormatProvider.cs
+++ CustomFormatProvider.Task2.Library/CustomFormatProvider.cs
@@ -12,21 +12,38 @@ namespace CustomFormatProvider.Task2.Library
         public object GetFormat(Type formatType)
         {
             if (formatType == typeof(ICustomFormatter))
-            {
                 return this;
-            }
             else
-            {
                 return null;
-            }
         }
 
         public string Format(string format, object arg, IFormatProvider formatProvider)
         {
-            
-            throw new NotImplementedException();
-        }
+            if (!this.Equals(formatProvider))
+                return null;
 
-        
+            if (string.IsNullOrEmpty(format))
+                format = "H";
+
+            string numericString = arg.ToString();
+
+            if (format == "H")
+            {
+                if (int.TryParse())
+                    return numericString;
+                else
+                    throw new FormatException(
+                              string.Format("'{0}' cannot be used to format {1}.",
+                                            format, arg.ToString()));
+            }
+            else 
+            {
+                throw new FormatException(string.Format("The {0} format specifier is invalid.", format));
+            }
+            
+            return numericString;
+
+
+        }
     }
 }
---
+++ Watch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomFormatProvider.Task2.Library;
+
+namespace Watch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HexadeсimalFormatter formatter = new HexadeсimalFormatter();
+        }
+    }
+}
