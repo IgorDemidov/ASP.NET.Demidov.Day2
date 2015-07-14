79c35d5bf7269ef4989bdddd7542977db727f611
IgorDemidov <igr.demidov@gmail.com>
2015-07-15 01:18:05 +0300

    ++

---
+++ Nod.Task3.Test/NodCalcTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nod.Task3.Library;
+
+namespace Nod.Task3.Test
+{
+    [TestClass]
+    public class NodCalcTest
+    {
+        [TestMethod]
+        public void NodCalcTest()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void NodCalcTimeTest()
+        {
+
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NodCalcExceptionTest()
+        {
+
+
+        }
+    }
+}
--- Nod/NodCalc.cs
+++ Nod/NodCalc.cs
@@ -10,14 +10,16 @@ namespace Nod.Task3.Library
     
     public static class NodCalc
     {
+       
         /// <summary>
         /// Computes the greatest common divisor (GCD) of two numbers
         /// </summary> 
-        public static int Evklidean(int first, int second, out TimeSpan timeElapsed)
+        public static int Evklidean(ref TimeSpan timeElapsed, int first, int second)
         {
             int result;
             int remainder;
             int temp;
+
             Stopwatch stopWatch = new Stopwatch();
 
             stopWatch.Start();
@@ -37,12 +39,32 @@ namespace Nod.Task3.Library
             } while (remainder != 0);
 
             stopWatch.Stop();
-            timeElapsed = stopWatch.Elapsed;
+            timeElapsed += stopWatch.Elapsed;
 
             result = first;
             return result;
         }
 
+        /// <summary>
+        /// Computes the greatest common divisor (GCD) of numbers set
+        /// </summary> 
+        public static int Evklidean(ref TimeSpan timeElapsed, params int[] args)
+        {
+            
+            if (args.Length<2)
+            {
+                throw new ArgumentNullException("args", "Unacceptable number of arguments");
+            }
+
+            int result = args[0];
+
+            for (int i = 0; i < args.Length-1; i++)
+            {
+                result = Evklidean(ref timeElapsed, result, args[i + 1]);
+            }
+   
+            return result;
+        }
       
     }
 }
