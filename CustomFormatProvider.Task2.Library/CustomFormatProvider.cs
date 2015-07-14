b60b8300a4f217de95c33c3476e8d149fdb688eb
IgorDemidov <igr.demidov@gmail.com>
2015-07-14 12:09:38 +0300

    BeginProvider

---
+++ CustomFormatProvider.Task2.Library/CustomFormatProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFormatProvider.Task2.Library
+{
+
+    public class HexadeсimalFormatter : IFormatProvider, ICustomFormatter
+    {
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
